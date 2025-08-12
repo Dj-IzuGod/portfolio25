a341bd86e4a0d27d2f6232633667fbdfd8a40f57
Nwali Izuchukwu Hosea <izuchukwunwali09@gmail.com>
2025-08-12 14:15:46 +0100

    updated lazy loading

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  // Wait for the entire page to load
-  setTimeout(function () {
-    document.querySelector(".loader-wrapper").style.display = "none";
-    document.querySelector("#root").style.display = "block";
-  }, 2000); // 1 second delay to ensure smooth transition (adjust to as needed)
-});
