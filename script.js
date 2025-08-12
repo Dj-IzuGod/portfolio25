d3144d40c458330a9ab29dee49f2545d716aceb9
Nwali Izuchukwu Hosea <izuchukwunwali09@gmail.com>
2025-08-12 14:05:10 +0100

    trying to solve the lazy loading bug

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.addEventListener("load", function () {
   // Wait for the entire page to load
   setTimeout(function () {
     document.querySelector(".loader-wrapper").style.display = "none";
-    document.querySelector(".content").style.display = "block";
+    document.querySelector("#root").style.display = "block";
   }, 2000); // 1 second delay to ensure smooth transition (adjust to as needed)
 });
